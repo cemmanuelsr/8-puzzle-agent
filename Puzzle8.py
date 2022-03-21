ce9bc5c49ecd7534f93c065f4646e5ee8e13c923
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-03-21 15:16:36 -0300

    mudando estrutura de files

--- Puzzle8.py
+++ Puzzle8.py
@@ -1,8 +1,5 @@
-from SearchAlgorithms import BuscaGananciosa, AEstrela, pretty_print_table
 from Graph import State
-
 from copy import deepcopy
-from time import time
 
 GOAL_TABLE = [
     [1, 2, 3],
@@ -21,20 +18,6 @@ TILE_GOALCOORDS_MAP = {
     8: (1, 0), 0: (1, 1), 4: (1, 2),
     7: (2, 0), 6: (2, 1), 5: (2, 2)
 }
-
-def flatten_table(table):
-    flatten = list()
-    for i in range(len(table)):
-        for j in range(len(table[0])):
-            flatten.append(table[i][j])
-
-    return flatten
-
-def get_algorithm(algorithm='BG'):
-    if(algorithm == 'BG'):
-        return 'Busca Gananciosa', BuscaGananciosa()
-    if(algorithm == 'A*'):
-        return 'A*', AEstrela()
 
 class Puzzle(State):
 
@@ -103,42 +86,3 @@ class Puzzle(State):
     def env(self):
         return f'{self.operator} ---> {self.table}'
 
-
-def main():
-
-    INITIAL_STATE = [
-        [1, 3, 4],
-        [8, 0, 5],
-        [7, 2, 6]
-    ]
-
-    flatten = flatten_table(INITIAL_STATE)
-    possible = 0
-    for i in range(len(flatten)):
-        for j in range(i+1, len(flatten)):
-            if(flatten[j] < flatten[i]):
-                possible += 1
-
-    if(possible%2):
-
-        state = Puzzle(INITIAL_STATE, (0, 0), '')
-        algo_name, algorithm = get_algorithm()
-        print(f'Estado inicial - {algo_name}')
-        pretty_print_table(INITIAL_STATE)
-        
-        start = time()
-        result = algorithm.search(state)
-        end = time()
-        if result != None:
-            print('Achou!')
-            print(result.show_path())
-        else:
-            print('Nao achou solucao')
-
-        print(f'Tempo de espera: {end - start} segundos')
-    
-    else:
-        print('Tabuleiro sem solução')
-
-if __name__ == '__main__':
-    main()
---
+++ main.py
@@ -0,0 +1,59 @@
+from Puzzle8 import Puzzle
+from SearchAlgorithms import BuscaGananciosa, AEstrela, pretty_print_table
+from Graph import State
+
+from copy import deepcopy
+from time import time
+
+def flatten_table(table):
+    flatten = list()
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            flatten.append(table[i][j])
+
+    return flatten
+
+def get_algorithm(algorithm='BG'):
+    if(algorithm == 'BG'):
+        return 'Busca Gananciosa', BuscaGananciosa()
+    if(algorithm == 'A*'):
+        return 'A*', AEstrela()
+
+def main():
+
+    INITIAL_STATE = [
+        [1, 3, 4],
+        [8, 0, 5],
+        [7, 2, 6]
+    ]
+
+    flatten = flatten_table(INITIAL_STATE)
+    possible = 0
+    for i in range(len(flatten)):
+        for j in range(i+1, len(flatten)):
+            if(flatten[j] < flatten[i]):
+                possible += 1
+
+    if(possible%2):
+
+        state = Puzzle(INITIAL_STATE, (0, 0), '')
+        algo_name, algorithm = get_algorithm()
+        print(f'Estado inicial - {algo_name}')
+        pretty_print_table(INITIAL_STATE)
+        
+        start = time()
+        result = algorithm.search(state)
+        end = time()
+        if result != None:
+            print('Achou!')
+            print(result.show_path())
+        else:
+            print('Nao achou solucao')
+
+        print(f'Tempo de espera: {end - start} segundos')
+    
+    else:
+        print('Tabuleiro sem solução')
+
+if __name__ == '__main__':
+    main()
