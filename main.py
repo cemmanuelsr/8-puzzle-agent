026d53621216cd414e1e1fa5396cdc979fa92c70
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-03-22 10:37:11 -0300

    api finalizada

--- main.py
+++ main.py
@@ -2,22 +2,46 @@ from Puzzle8 import Puzzle
 from utils import initial_state, get_algorithm
 
 from fastapi import FastAPI, HTTPException
-from typing import Optional
 from enum import Enum
 from time import time
 
 app = FastAPI()
 
 class Algorithm(str, Enum):
-    BuscaGananciosa: "bg"
-    AEstrela: "star"
+    bg = "BuscaGananciosa"
+    star = "A*"
 
 class Heuristic(str, Enum):
-    Manhattan: "manhattan"
-    Euclidian: "euclidian"
-    MatchPositions: "match"
-    ManhattanBetter: "manhattan2"
+    manhattan2 = "ManhattanBetter"
+    manhattan = "Manhattan"
+    euclidian = "Euclidian"
+    match = "MatchPositions"
 
-@app.get('/solve/')
-async def solve(table : str, algorithm : Optional[Algorithm], heuristic : Optional[Heuristic]):
-    pass
+@app.get('/solve/{algorithm}/{heuristic}')
+async def solve(table : str, algorithm : Algorithm, heuristic : Heuristic):
+    initial_table = initial_state(table)
+    if(heuristic):
+        state = Puzzle(initial_table, (0, 0), '', heuristic.name)
+    else:
+        state = Puzzle(initial_table, (0, 0), '')
+
+    if(not state.is_possible()):
+        raise HTTPException(status_code = 404, detail = 'Not possible solution')
+
+    if(algorithm):
+        algo_name, algo = get_algorithm(algorithm.name)
+    else:
+        algo_name, algo = get_algorithm()
+    start = time()
+    result = algo.search(state)
+    end = time()
+    
+    if(result != None):
+        return { 
+            "path": result.show_path(),
+            "time": end - start,
+            "algorithm_name": algo_name,
+            "heuristic_name": state.heuristic
+        }
+    else:
+        raise HTTPException(status_code = 404, detail = 'Not found solution')
