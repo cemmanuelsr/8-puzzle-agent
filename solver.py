4304bb8174024e85b6793e00fd91cbeb5fbcfff0
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-03-22 09:53:58 -0300

    trocando arquitetura de nomes dos arquivos para acomodar uvicorn

---
+++ solver.py
@@ -0,0 +1,48 @@
+from Puzzle8 import Puzzle
+from SearchAlgorithms import BuscaGananciosa, AEstrela, pretty_print_table
+
+from time import time
+import argparse
+
+parser = argparse.ArgumentParser(prog='main')
+parser.add_argument('-a', '--algorithm', type=str, default='bg')
+parser.add_argument('-H', '--heuristic', type=str, default='manhattan2')
+parser.add_argument('-s', '--state', type=str, default='2, 8, 3, 1, 4, 0, 7, 6, 5')
+args = parser.parse_args()
+
+def initial_state(string_state):
+    tiles = [int(tile) for tile in string_state.split(',')]
+    table = [tiles[i:i+3] for i in range(0, len(tiles), 3)]
+
+    return table
+
+def get_algorithm(algorithm='bg'):
+    if(algorithm == 'bg'):
+        return 'Busca Gananciosa', BuscaGananciosa()
+    if(algorithm == 'star'):
+        return 'A*', AEstrela()
+
+def main():
+
+    INITIAL_STATE = initial_state(args.state)
+    state = Puzzle(INITIAL_STATE, (0, 0), '', args.heuristic)
+
+    if(state.is_possible()):
+
+        algo_name, algorithm = get_algorithm(args.algorithm)
+        print(f'Estado inicial - Realizando {algo_name} c/ heuristica {state.heuristic}')
+        pretty_print_table(INITIAL_STATE)
+        
+        start = time()
+        result = algorithm.search(state)
+        end = time()
+        if result != None:
+            print('Achou!')
+            print(result.show_path())
+        else:
+            print('Nao achou solucao')
+
+        print(f'Tempo de espera: {end - start} segundos')
+
+if __name__ == '__main__':
+    main()
